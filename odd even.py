6560e192c250d955a7f5862c3483ce3fb9f89c1e
Amey Joshi <amey.joshi1999@gmail.com>
2016-10-08 11:51:06 +0530

    message

---
+++ odd even.py
@@ -0,0 +1,9 @@
+def is even(number)
+    if number%2==0
+       print('the number is an even mumber')
+     else:
+          print('this is an odd number')
+num=int(input('enter a number'))
+is even(num)
+          
+          
